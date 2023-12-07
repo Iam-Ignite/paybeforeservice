416266091671343a7cf80922e7411e43b7c21d83
Prosper <nkonyeprospero@gmail.com>
2023-12-07 15:12:12 +0100

    fixed up some errors

--- src/components/Dashboard/Refcomp/Txref.jsx
+++ src/components/Dashboard/Refcomp/Txref.jsx
@@ -1,69 +1,76 @@
-import React from 'react';
-import { Txbalance, Txbutton, Txdate, Txicon, Txstatus, Txtype } from '../Transactioncomp/Txcomp';
+import React from "react";
+import {
+  Txbalance,
+  TxReedem,
+  Txdate,
+  TxiconIn,
+  TxiconOut,
+  Txstatus,
+  Txtype,
+} from "../Transactioncomp/Txcomp";
 
 export default function Txref() {
-
-   const tx = [
+  const tx = [
     {
-        type: "Recieved from client",
-        tx_id: "567773DTYY373",
-        status: "pending",
-        date: "2023-09-01T10:00:00Z",
-        amount: 5000
+      type: "Recieved from client",
+      tx_id: "567773DTYY373",
+      status: "pending",
+      date: "2023-09-01T10:00:00Z",
+      amount: 5000,
     },
     {
-        type: "Recieved from client",
-        tx_id: "567773DTYY373",
-        status: "pending",
-        date: "2023-09-01T10:00:00Z",
-        amount: 5000
+      type: "Recieved from client",
+      tx_id: "567773DTYY373",
+      status: "pending",
+      date: "2023-09-01T10:00:00Z",
+      amount: 5000,
     },
     {
-        type: "Recieved from client",
-        tx_id: "567773DTYY373",
-        status: "pending",
-        date: "2023-09-01T10:00:00Z",
-        amount: 5000
+      type: "Recieved from client",
+      tx_id: "567773DTYY373",
+      status: "pending",
+      date: "2023-09-01T10:00:00Z",
+      amount: 5000,
     },
     {
-        type: "Recieved from client",
-        tx_id: "567773DTYY373",
-        status: "pending",
-        date: "2023-09-01T10:00:00Z",
-        amount: 5000
+      type: "Recieved from client",
+      tx_id: "567773DTYY373",
+      status: "pending",
+      date: "2023-09-01T10:00:00Z",
+      amount: 5000,
     },
     {
-        type: "Recieved from client",
-        tx_id: "567773DTYY373",
-        status: "pending",
-        date: "2023-09-01T10:00:00Z",
-        amount: 5000
-    },    {
-        type: "Recieved from client",
-        tx_id: "567773DTYY373",
-        status: "pending",
-        date: "2023-09-01T10:00:00Z",
-        amount: 5000
-    }
-   ]
+      type: "Recieved from client",
+      tx_id: "567773DTYY373",
+      status: "pending",
+      date: "2023-09-01T10:00:00Z",
+      amount: 5000,
+    },
+    {
+      type: "Recieved from client",
+      tx_id: "567773DTYY373",
+      status: "pending",
+      date: "2023-09-01T10:00:00Z",
+      amount: 5000,
+    },
+  ];
 
   return (
-    <div className='flex justify-between px-2 items-center'>
-        {/* tx icon and type */}
-       <div className="flex items-center gap-2">
+    <div className="flex justify-between px-2 items-center">
+      {/* tx icon and type */}
+      <div className="flex items-center gap-2">
         {/* Icon */}
-          <Txicon />
+        <Txicon />
 
         {/* Type */}
         <div className="text-[#555] leading-[20px]">
-            <Txtype />
-            <Txdate />
+          <Txtype />
+          <Txdate />
         </div>
-       </div>
-  
-       {/* Amount */}
-       <Txbalance />
+      </div>
 
+      {/* Amount */}
+      <Txbalance />
     </div>
-  )
+  );
 }
